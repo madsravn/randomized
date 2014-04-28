f7708a07af2a3961832bcdbc64e0a59ae0a6430f
Johan Abildskov <randomsort@gmail.com>
2014-04-28 13:41:45 +0200

    Added more tests on each list, both find and quicksort

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -66,7 +66,7 @@ int find(std::vector<int> L, int k, std::mt19937& gen, uint64& depth, uint64& co
 }
 
 
-std::vector<int> quicksort(std::vector<int> L, std::mt19937& gen, int& depth, int& comparisons)
+std::vector<int> quicksort(std::vector<int> L, std::mt19937& gen, uint64& depth, uint64& comparisons)
 {
 	std::vector<int> L1, L2;
 	++depth;
@@ -103,8 +103,11 @@ int testingonetwo(const int powerofn) {
             gen.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
             uint64 comps = 0;
             uint64 depth = 0;
-
+	    Timer gen_t;
+	    gen_t.start();
             std::vector<int> integers = generate_integers(n);
+	    gen_t.stop();
+	    std::cout << "generated " << n << " integers in " << gen_t.duration().count() << " ms" << std::endl;
             Timer t;
             t.start();
             find(integers, k, gen, depth, comps);
@@ -116,7 +119,51 @@ int testingonetwo(const int powerofn) {
     }
     return 1;
 }
-         
+
+int test_k_runs(const int powerofn)
+{
+	for(int pon = 2; pon < powerofn+1;++pon)
+	{
+		int n = pow(10,pon);
+		std::vector<int> ks{1};
+		for(int i=2; i<10; ++i)
+		{
+			ks.push_back((n/10)*i);
+		}
+		for(const auto& i : ks)
+		{
+			//Generate a list of random numbers for each k, test each k in each list, both quicksort and k find.
+			Timer gt;
+			gt.start();
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			gen.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
+			std::vector<int> integers = generate_integers(n);
+			gt.stop();
+			std::cout << "generated " << n << " integers in " << gt.duration().count() << " ms" << std::endl;
+
+			//For each k lists, run through the list for each k.
+			for(const auto& k : ks)
+			{
+				uint64 comps = 0;
+				uint64 depth = 0;
+				Timer ft;
+				ft.start();
+				find(integers, k, gen, depth, comps);
+				ft.stop();
+				std::cout << "find - n: " << n << " and k: " << k << " took " << ft.duration().count() << " ms and had depth: " << depth << " and comps: " << comps << std::endl;
+			}
+			uint64 comps = 0;
+			uint64 depth = 0;
+			Timer qt;
+			qt.start();
+			quicksort(integers,gen, depth, comps);
+			qt.stop();
+			std::cout << "sort -n: " << n << " took " << qt.duration().count() << " ms and had depth: " << depth << " and comps: " << comps << std::endl;
+		}
+	}
+	return 1;
+}
 int main() {
 
 
@@ -132,12 +179,13 @@ int main() {
     //    std::cout << find(integers, integers.size()/3, gen, depth, comps) << std::endl;
     //std::cout << "comps: " << comps << " and depth: " << depth << std::endl;
     comps = depth =0;
-    std::cout << quicksort(integers, gen, depth, comps) << std::endl;
-    std::cout << "comps: " << comps << " and depth: " << depth << std::endl;
+    //std::cout << quicksort(integers, gen, depth, comps) << std::endl;
+    //std::cout << "comps: " << comps << " and depth: " << depth << std::endl;
     comps = depth = 0;
     //std::cout << find(integers, integers.size()/3, gen, depth, comps) << std::endl;
     //std::cout << "comps: " << comps << " and depth: " << depth << std::endl; 
-    testingonetwo(5);
-
+    std::cout << "hg find:" << std::endl;
+//    testingonetwo(8);h
+    test_k_runs(4);
     return 0;
 }
