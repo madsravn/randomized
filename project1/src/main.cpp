6d2cfd3dc20c768fed005b11dcf6e66d6fff9d6a
Mads Ravn <madsravn@gmail.com>
2014-04-30 20:33:00 +0200

    Globally random generated numbers and fixed list of random integers

--- project1/src/main.cpp
+++ project1/src/main.cpp
@@ -9,6 +9,10 @@
 #include "Timer.hpp"
 
 typedef unsigned long long uint64;
+std::vector<int> currentlist;
+std::mt19937 gen;
+
+
 
 template <class T>
 inline std::ostream& operator << (std::ostream& os, const std::vector<T>& v) 
@@ -41,7 +45,7 @@ std::vector<int> generate_integers(int size) {
 
 
 
-int find(std::vector<int> L, int k, std::mt19937& gen, uint64& depth, uint64& comparisons) {
+int find(std::vector<int> L, int k, uint64& depth, uint64& comparisons) {
 	std::vector<int> L1, L2;
 	++depth;
 
@@ -59,14 +63,14 @@ int find(std::vector<int> L, int k, std::mt19937& gen, uint64& depth, uint64& co
 		return e;
 	}
 	if (L1.size() > (k - 1)) {
-		return find(L1, k, gen, depth, comparisons);
+		return find(L1, k, depth, comparisons);
 	} else {
-		return find(L2, k - 1 - L1.size(), gen, depth, comparisons);
+		return find(L2, k - 1 - L1.size(), depth, comparisons);
 	}
 }
 
 
-std::vector<int> quicksort(std::vector<int> L, std::mt19937& gen, uint64& depth, uint64& comparisons)
+std::vector<int> quicksort(std::vector<int> L, uint64& depth, uint64& comparisons)
 {
 	std::vector<int> L1, L2;
 	++depth;
@@ -80,9 +84,9 @@ std::vector<int> quicksort(std::vector<int> L, std::mt19937& gen, uint64& depth,
 	L.erase(L.begin() + pos);
 	std::partition_copy(std::begin(L), std::end(L), std::back_inserter(L1), std::back_inserter(L2), 
 			[&](int i){++comparisons; return i<e;});
-	L1 = quicksort(L1, gen, depth, comparisons);
+	L1 = quicksort(L1, depth, comparisons);
         L1.push_back(e);
-	L2 = quicksort(L2, gen, depth, comparisons);
+	L2 = quicksort(L2, depth, comparisons);
 	L1.insert(L1.end(), L2.begin(), L2.end());
 	return L1;
 
@@ -92,25 +96,18 @@ int testingonetwo(const int powerofn) {
 
     for(int pon = 2; pon < powerofn+1; ++pon) {
         int n = pow(10, pon);
+        currentlist = generate_integers(n);
         std::vector<int> ks {1};
         for(int i = 2; i < 10; ++i) {
             ks.push_back((n/10)*i);
         }
         for(const auto& k : ks) {
-            std::random_device rd;
-            std::mt19937 gen(rd());
 
-            gen.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
             uint64 comps = 0;
             uint64 depth = 0;
-	    Timer gen_t;
-	    gen_t.start();
-            std::vector<int> integers = generate_integers(n);
-	    gen_t.stop();
-	    std::cout << "generated " << n << " integers in " << gen_t.duration().count() << " ms" << std::endl;
             Timer t;
             t.start();
-            find(integers, k, gen, depth, comps);
+            find(currentlist, k, depth, comps);
             t.stop();
             std::cout << "n: " << n << " and k: " << k << " took " << t.duration().count() << " ms and had depth: " << depth << " and comps: " << comps << std::endl;
 
@@ -125,6 +122,7 @@ int test_k_runs(const int powerofn)
 	for(int pon = 2; pon < powerofn+1;++pon)
 	{
 		int n = pow(10,pon);
+        currentlist = generate_integers(n);
 		std::vector<int> ks{1};
 		for(int i=2; i<10; ++i)
 		{
@@ -132,15 +130,6 @@ int test_k_runs(const int powerofn)
 		}
 		for(const auto& i : ks)
 		{
-			//Generate a list of random numbers for each k, test each k in each list, both quicksort and k find.
-			Timer gt;
-			gt.start();
-			std::random_device rd;
-			std::mt19937 gen(rd());
-			gen.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
-			std::vector<int> integers = generate_integers(n);
-			gt.stop();
-			std::cout << "generated " << n << " integers in " << gt.duration().count() << " ms" << std::endl;
 
 			//For each k lists, run through the list for each k.
 			for(const auto& k : ks)
@@ -149,7 +138,7 @@ int test_k_runs(const int powerofn)
 				uint64 depth = 0;
 				Timer ft;
 				ft.start();
-				find(integers, k, gen, depth, comps);
+				find(currentlist, k, depth, comps);
 				ft.stop();
 				std::cout << "find - n: " << n << " and k: " << k << " took " << ft.duration().count() << " ms and had depth: " << depth << " and comps: " << comps << std::endl;
 			}
@@ -157,7 +146,7 @@ int test_k_runs(const int powerofn)
 			uint64 depth = 0;
 			Timer qt;
 			qt.start();
-			quicksort(integers,gen, depth, comps);
+			quicksort(currentlist, depth, comps);
 			qt.stop();
 			std::cout << "sort -n: " << n << " took " << qt.duration().count() << " ms and had depth: " << depth << " and comps: " << comps << std::endl;
 		}
@@ -167,25 +156,9 @@ int test_k_runs(const int powerofn)
 int main() {
 
 
-    std::vector<int> integers = generate_integers(100);
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
     // Ifølge dokumentationen for random_device er det ikke sikkert den er implementeret ved alle compilere endnu, så vi seeder lige med tiden oveni.
     gen.seed(static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()));
-    int comps = 0;
-    int depth = 0;
 
-    //    std::cout << find(integers, integers.size()/3, gen, depth, comps) << std::endl;
-    //std::cout << "comps: " << comps << " and depth: " << depth << std::endl;
-    comps = depth =0;
-    //std::cout << quicksort(integers, gen, depth, comps) << std::endl;
-    //std::cout << "comps: " << comps << " and depth: " << depth << std::endl;
-    comps = depth = 0;
-    //std::cout << find(integers, integers.size()/3, gen, depth, comps) << std::endl;
-    //std::cout << "comps: " << comps << " and depth: " << depth << std::endl; 
-    std::cout << "hg find:" << std::endl;
-//    testingonetwo(8);h
     test_k_runs(4);
     return 0;
 }
