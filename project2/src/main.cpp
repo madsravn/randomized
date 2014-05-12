06f354a31da90722a2f99471d0afdcc156997ecf
Mads Ravn <madsravn@gmail.com>
2014-05-12 19:43:19 +0200

    Rettede main for fremtidig sikring

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-using namespace std;
-int main(int argc, char* argv[])
-{
+#include <iostream>
 
-	printf("Hello world!\n");
+
+int main(int argc, char** argv) {
+
+    std::cout << "Hello world!" << std::endl;
+
 	return 0;
 }
