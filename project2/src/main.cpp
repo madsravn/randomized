82981e761980ae57495aedee6fe531796019eb98
Mads Ravn <madsravn@gmail.com>
2014-05-12 19:57:49 +0200

    Type checking stuff

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// If we need a better precision data type later
+typedef char character;
+// Guard against weird compiler implementation
+static_assert(sizeof(character) <= 2, "This data type exceeds the 16 bit bound");
 
 int main(int argc, char** argv) {
 
