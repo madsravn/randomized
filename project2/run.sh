6d42ae07815e23e1d7f054302d886648a44ef1ce
Johan Abildskov <fx@cs.au.dk>
2014-05-26 23:56:11 +0200

    report and run.sh -> build directory

--- project2/run.sh
+++ project2/run.sh
@@ -2,16 +2,16 @@
 
 for i in $(seq 1 7); do
     A="data""$i""a"
-    if [[ ! -f tests/$A ]]; then
+    if [[ ! -f build/$A ]]; then
         echo "File doesn't exists"
         DL="http://users-cs.au.dk/gudmund/randomF14/multisets/""$A"
-        wget "$DL" -O tests/$A
+        wget "$DL" -O build/$A
     fi
     B="data""$i""b"
-    if [[ ! -f tests/$B ]]; then
+    if [[ ! -f build/$B ]]; then
         echo "File doesn't exists"
         DL="http://users-cs.au.dk/gudmund/randomF14/multisets/""$B"
-        wget "$DL" -O tests/$B
+        wget "$DL" -O build/$B
     fi
 d
 done
